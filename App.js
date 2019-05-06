0472d8480aeefcf0319973c63e3586e4c2e79ce0
Matheus Souza <matheu.sk.9@hotmail.com>
2019-05-06 13:15:37 -0300

    about to give up on this shit

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SectionList
+} from 'react-native';
 
-export default class App extends React.Component {
+export default class MyList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sectionData: []
+    };
+  }
+
+  componentWillMount() {
+    fetch('https://api.randomuser.me/?nat=US&results=6')
+      .then(response => response.json())
+      .then(responseJson => {
+        let sectionData = [];
+        responseJson.results.forEach(person => {
+          sectionData.push({
+            title: person.name.first,
+            data: [person.email]
+          });
+        });
+        this.setState({
+          sectionData
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <SectionList
+          sections={this.state.sectionData}
+          renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({ section }) => (
+            <Text style={styles.sectionHeader}>{section.title}</Text>
+          )}
+          keyExtractor={(item, index) => index}
+        />
       </View>
     );
   }
@@ -14,8 +53,20 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 22
   },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)'
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44
+  }
 });
